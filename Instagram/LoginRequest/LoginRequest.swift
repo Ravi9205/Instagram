f21dd03989b52383615012753c655ac04585f230
Ravi dwivedi <ravi.dwivedi9205@gmail.com>
2022-05-14 17:38:39 +0530

    Login Module impletions basic UI setup validations and MVVM architecture

---
+++ Instagram/LoginRequest/LoginRequest.swift
@@ -0,0 +1,16 @@
+//
+//  LoginRequest.swift
+//  Instagram
+//
+//  Created by Ravi Dwivedi on 13/05/22.
+//
+
+import Foundation
+
+
+struct LoginRequest {
+    
+    var email:String?
+    var password:String?
+    
+}
---
+++ Instagram/Validation/ValidationResult/ValidationResult.swift
@@ -0,0 +1,16 @@
+//
+//  ValidationResult.swift
+//  Instagram
+//
+//  Created by Ravi Dwivedi on 13/05/22.
+//
+
+import Foundation
+
+
+struct ValidationResult {
+    
+    var success:Bool
+    var  error : String?
+    
+}
