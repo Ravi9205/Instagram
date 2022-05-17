6ed65f9ee866062fd7e9e301a6b18064e9ff9876
Ravi dwivedi <ravi.dwivedi9205@gmail.com>
2022-05-17 23:19:08 +0530

    Profile UI and Notification setup added Logging out feature

--- Instagram/Controllers/NotificationVC/NotificationViewController.swift
+++ Instagram/Controllers/NotificationVC/NotificationViewController.swift
@@ -8,14 +8,47 @@
 import UIKit
 
 class NotificationViewController: UIViewController {
-
+    
+    private var tableView:UITableView = {
+        let tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier:"Cell")
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        self.title = "Notification"
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        
     }
     
-
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
     
+}
 
+//MARK:- TableView Delegate Data Source
+extension NotificationViewController:UITableViewDelegate, UITableViewDataSource{
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier:"Cell", for:indexPath)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
 }
